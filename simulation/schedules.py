79427df08990bd85310831385ee4d5a8ed5e5e1d
Milind Kumar V <milindkumarvaddiraju@gmail.com>
2024-05-24 10:19:16 -0500

    Add setup for dynamic max weight experiment

--- simulation/schedules.py
+++ simulation/schedules.py
@@ -58,6 +58,12 @@ def create_schedule(UE_names: list, start_time: float, end_time: float, schedule
         num_UEs_together = schedule_config["num_UEs_together"]
         assert num_UEs_together < len(UE_names), "Number of UEs together should be less than \
              or equal to the total number of UEs"
+        
+        if "offset" in schedule_config:
+            offset = schedule_config["offset"]
+            assert offset > 0, "Offset should be greater than 0"
+        else:
+            offset = 1
 
         num_UEs = len(UE_names)
         slots_temp = {}
@@ -67,7 +73,7 @@ def create_schedule(UE_names: list, start_time: float, end_time: float, schedule
             qbv_end_time = min(qbv_start_time + qbv_window_size, end_time)
             UE_names_temp = []
             for i in range(num_UEs_together):
-                UE_names_temp.append(UE_names[((num_slot + i) % num_UEs)])
+                UE_names_temp.append(UE_names[((num_slot + i*offset) % num_UEs)])
             slots_temp[num_slot] = Slot(num_slot,\
                                         qbv_start_time,\
                                         qbv_end_time,
@@ -269,6 +275,49 @@ def create_schedule(UE_names: list, start_time: float, end_time: float, schedule
 
         return (schedule_contention, 10*cycle_time)
     
+    elif schedule_name == "contention 5 apart 10UEs":
+
+        assert "qbv_window_size" in schedule_config, "Round Robin schedule requires 'qbv_window_size' parameter"
+        qbv_window_size = schedule_config["qbv_window_size"]
+
+        num_UEs = len(UE_names)
+        slots_temp = {}
+        qbv_start_time = start_time
+        num_slot = 0
+
+        contention_counter = 0
+
+        while qbv_start_time < end_time:
+            qbv_end_time = min(qbv_start_time + qbv_window_size, end_time)
+            UE_names_temp = []
+
+            if num_slot%5 == 0:
+                contention_counter += 1
+                if (num_slot/5)%2 == 0:
+                    UE_names_temp = ["UE4", "UE5"]
+                else:
+                    UE_names_temp = ["UE0", "UE9"]
+            else:
+                UE_names_temp = [UE_names[((num_slot - contention_counter) % num_UEs)]]
+
+
+            slots_temp[num_slot] = Slot(num_slot,\
+                                        qbv_start_time,\
+                                        qbv_end_time,
+                                        "contention",
+                                        UE_names_temp)
+            num_slot += 1
+            qbv_start_time = qbv_end_time
+
+            if num_slot == num_UEs+2:
+                cycle_time = qbv_end_time 
+
+        schedule_contention = Schedule(start_time, end_time, num_slot, slots_temp)
+
+        return (schedule_contention, 10*cycle_time)
+
+    
+    
     
     
 
@@ -316,6 +365,55 @@ def create_schedule_dynamic(UE_names: list, start_time: float, end_time: float, 
             qbv_end_time = min(qbv_start_time + qbv_window_size, end_time)
             UE_name = UE_names[(num_slot % num_UEs)]
             UE_names_temp = [UE_name]
+            slots_temp[num_slot] = Slot(num_slot,\
+                                        qbv_start_time,\
+                                        qbv_end_time,
+                                        "contention",
+                                        UE_names_temp)
+            num_slot += 1
+            qbv_start_time = qbv_end_time
+
+            if num_slot == num_UEs:
+                cycle_time = qbv_end_time 
+
+        schedule_contention = Schedule(start_time, end_time, num_slot, slots_temp)
+
+        return (schedule_contention, cycle_time)
+    
+    if schedule_name == "dynamic roundrobin blank":
+
+        num_UEs = len(UE_names)
+        wifi_slot_time = config["wifi_slot_time"] # microseconds
+        DIFS = config["DIFS"] # microseconds
+        CWmin = config["CWmin"]
+        slots_temp = {}
+        qbv_start_time = start_time
+        num_slot = 0
+
+        # computing the qbv_window_size based on the lambda value and delivery latency
+        for delivery_latency_index in range(len(delivery_latency)):
+            delivery_latency_value = delivery_latency[delivery_latency_index]
+            slot_length_temp = DIFS + CWmin*wifi_slot_time + delivery_latency_value
+            if num_UEs*slot_length_temp*lambda_value < delivery_latency_index:
+                qbv_window_size = slot_length_temp
+                break
+            elif delivery_latency_index == len(delivery_latency) - 1:
+                qbv_window_size = slot_length_temp
+
+
+        # Create the first slot with all the UEs
+        slots_temp[num_slot] = Slot(num_slot,\
+                                    qbv_start_time,\
+                                    qbv_start_time + qbv_window_size,
+                                    "contention",
+                                    UE_names)
+        num_slot += 1
+        qbv_start_time += qbv_window_size
+
+
+        while qbv_start_time < end_time:
+            qbv_end_time = min(qbv_start_time + qbv_window_size, end_time)
+            UE_names_temp = []
             slots_temp[num_slot] = Slot(num_slot,\
                                         qbv_start_time,\
                                         qbv_end_time,
