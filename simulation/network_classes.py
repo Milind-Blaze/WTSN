960191829fc2ccde73bec7f2cc8dffaedab9f746
Milind Kumar V <milindkumarvaddiraju@gmail.com>
2024-06-20 13:45:50 -0500

    Remove max weight functionality for an empty array as this is interfering with the half HVC schedule

--- simulation/network_classes.py
+++ simulation/network_classes.py
@@ -973,24 +973,25 @@ class Network:
 
                     # Measure queues at the start of the slot
                     # for UE_name in UEs_all:
-                    max_queue_length = 0
+                    # max_queue_length = 0
                     queue_lengths_this_slot = []
                     for UE_name in UEs_all:
                         queue_length = bisect.bisect_right(arrival_times[UE_name], start_time)
                         queue_lengths_this_slot.append(queue_length)
                         self.UEs[UE_name].transmission_record[slot]["queue_information"]["queue_lengths"].append(queue_length)
                         self.UEs[UE_name].transmission_record[slot]["queue_information"]["queue_times"].append(start_time)
-                        if queue_length > max_queue_length:
-                            max_queue_length = queue_length
+                        # if queue_length > max_queue_length:
+                        #     max_queue_length = queue_length
                     
                     if len(UEs_to_contend) == 0:
-                        if max_queue_length == 0:
-                            UEs_to_contend = copy.deepcopy(UEs_all)
-                        else:
-                            for UE_name in UEs_all:
-                                if self.UEs[UE_name].transmission_record[slot]["queue_information"]["queue_lengths"][-1] == max_queue_length:
-                                    UEs_to_contend.append(UE_name)
-                                    break
+                        print("UEs_to_contend is empty")
+                        # if max_queue_length == 0:
+                        #     UEs_to_contend = copy.deepcopy(UEs_all)
+                        # else:
+                        #     for UE_name in UEs_all:
+                        #         if self.UEs[UE_name].transmission_record[slot]["queue_information"]["queue_lengths"][-1] == max_queue_length:
+                        #             UEs_to_contend.append(UE_name)
+                        #             break
                         if self.debug_mode:
                             print("\n\nstart time: ", start_time)
                             print("UEs to contend after max weight: ", UEs_to_contend)
