c67b8dbba8e54272d27ece54d0ebf4c5e532f526
Milind Kumar V <milindkumarvaddiraju@gmail.com>
2024-06-24 15:12:08 -0500

    Fix name bug

--- simulation/simulation4/experiment_scripts/rr_hotnets_dummy.sh
+++ simulation/simulation4/experiment_scripts/rr_hotnets_dummy.sh
@@ -9,6 +9,6 @@ for FILE in "$DIRECTORY"/*; do
   if [ -f "$FILE" ]; then
     FILENAME=$(basename "$FILE")
     echo "$FILENAME"
-    python3 simulation4.py ../wireless_parameters/wireless_parameters_DL_MU_964B_80Mhz_Nss2_98usBA.json "../experiment_configs/hotnets_dummy_results/$FILENAME" &> "logs/$FILENAME.log"
+    python3 simulation4.py ../wireless_parameters/wireless_parameters_DL_MU_964B_80Mhz_Nss2_98usBA.json "../experiment_configs/hotnets_dummy_results/roundrobin/$FILENAME" &> "logs/$FILENAME.log"
   fi
 done
